e50705069b445a6c314a5e2eee41541848cf0172
Rubel <rubel190024@gmail.com>
2024-11-03 23:48:23 +0600

    done

---
+++ myscripts.js
@@ -0,0 +1,8 @@
+window.addEventListener("mouseover", play);
+window.addEventListener("click", play);
+function play() {
+  document.getElementById("a").play();
+}
+//alert ("I don’t have words to describe what I feel for you. Meeting you is the best thing that ever happened in my life. Happy Birthday!☺️");
+
+//alert ("important --> click on screen 4 times in 4 second ");
